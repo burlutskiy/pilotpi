894b2af88c7a0a64c365972b6d9634c38178c782
burlutskiy <burlutskiy.a.s@gmail.com>
2014-05-09 11:42:14 -0400

    commin from tatiana

---
+++ core/src/main/java/org/pilotpi/math/Matrix.java
@@ -0,0 +1,27 @@
+package org.pilotpi.math;
+
+public class Matrix {
+
+	private float[][] matrix = {
+			{0,0,0},
+			{0,0,0},
+			{0,0,0}
+		};
+	public Matrix(Vector vector1, Vector vector2, Vector vector3) {
+		for (int i = 0; i < 3; i++) {
+			matrix[0][i]+=vector1.intern()[i];
+		}
+		for (int i = 0; i < 3; i++) {
+			matrix[1][i]+=vector2.intern()[i];
+		}
+		for (int i = 0; i < 3; i++) {
+			matrix[2][i]+=vector3.intern()[i];
+		}
+	}
+	public Matrix() {
+	}
+	public Vector getRow(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
